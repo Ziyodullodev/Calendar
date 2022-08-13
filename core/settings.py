585afc301fc1e160b3f2ec83d5789aa10b95f30f
Ziyodullo Aliyev <89968839+Ziyodullodev@users.noreply.github.com>
2022-08-13 10:52:35 +0500

    hostga yuklashga debugni ochirdim

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ry*y#9^y3mpbf76i8kq)=rnh#$02zk!dqgof!&4qqhb(4_o*9u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
