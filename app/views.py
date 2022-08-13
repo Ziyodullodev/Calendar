8ebf932449eea1d0018abd1e8a1ab6c5b754ebbc
Ziyodullo Aliyev <89968839+Ziyodullodev@users.noreply.github.com>
2022-08-13 10:48:12 +0500

    print qolib ketgan ekan ochirdim

--- app/views.py
+++ app/views.py
@@ -14,11 +14,11 @@ def Calendar(request):
     )
     kun = f'<td class="{x.strftime("%a").lower()}">{x.day}</td>'
     oy = f'<tr><th colspan="7" class="month">{x.strftime("%B")} {x.year}</th></tr>'
-    print(kun)
+    # print(kun)
     # for i in cal:
     cal = cal.replace(kun, f'<td class="today">{x.day}</td>')
     cal = cal.replace(oy, f'<tr><th colspan="7" class="month"><a href="/search/{x.year}/{x.month-1}"><<<</a> {x.strftime("%B")} {x.year} <a href="/search/{x.year}/{x.month+1}">>>></a> </th></tr>')
-    print(type(cal))
+    # print(type(cal))
     data = datetime.datetime.strptime('2018-06-21', '%Y-%m-%d')
     context = {
         "time":"time",
@@ -47,13 +47,13 @@ def Search(request, oy, year):
     kun = f'<td class="{b.strftime("%a").lower()}">{b.day}</td>'
     cal = cal.replace(kun, f'<td class="today">{b.day}</td>')
     cal = cal.replace(oy, f'<tr><th colspan="7" class="month"><a href="/search/{x.year}/{x.month-1}"><<<</a> {x.strftime("%B")} {x.year} <a href="/search/{x.year}/{x.month+1}">>>></a> </th></tr>')
-    
+
     context = {
         "time":"time",
         # "month":month,
         "calendar":cal,
         # "year":oy
     }
-    print(x.strftime("%B"))
-    print(oy)
+    # print(x.strftime("%B"))
+    # print(oy)
     return render(request, 'calendar.html', context=context)
