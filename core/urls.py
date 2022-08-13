a9ea132c5eb50371acd6b4074218a2236843b2c5
Ziyodullo Aliyev <89968839+Ziyodullodev@users.noreply.github.com>
2022-08-13 11:00:02 +0500

    settinsni ozgartirdim

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('ziyodullo/', admin.site.urls),
     path('', include('app.urls')),
 ]
